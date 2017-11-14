c48b6b1368263cad6957f4dc6b21fea988825654
Bill Good <bill.good@respec.com>
2017-11-14 10:13:55 -0700

    First post!

---
+++ MyConsoleApp/MyConsoleApp.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World.");
+            System.Console.WriteLine("Press any key to quit.");
+            System.Console.ReadKey();
+        }
+    }
+}
